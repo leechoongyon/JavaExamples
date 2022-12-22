43366b9c65915757748c79198db7ea214ce036ae
LeeJunGyun <rainmaker0303@hanmail.net>
2022-12-22 23:53:21 +0900

    java funtional

---
+++ src/test/java/org/example/function/FunctionalInterfaceTest.java
@@ -0,0 +1,49 @@
+package org.example.function;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class FunctionalInterfaceTest {
+
+    @Test
+    public void functionalInterfaceTest01() throws Exception {
+        FunctionalInterfaceSample func = message -> System.out.println(message);
+        func.helloWorld("HelloWorld...");
+    }
+
+    @Test
+    public void supplierFunctionalInterfaceTest() throws Exception {
+        Supplier<String> getMessage = () -> "Hello World...";
+        Assertions.assertEquals("Hello World...", getMessage.get());
+    }
+
+    @Test
+    public void consumerFunctionalInterfaceTest() throws Exception {
+        Consumer<String> setMessage = message -> System.out.println(message);
+        setMessage.accept("Hello World...");
+    }
+
+    @Test
+    public void functionFuntionalInterfaceTest() throws Exception {
+        Function<String, String> appendPrefixSuffix = (str) -> "###" + str + "###";
+        Assertions.assertEquals("###Hello World###", appendPrefixSuffix.apply("Hello World"));
+
+    }
+
+    @Test
+    public void predicateFunctionalInterfaceTest() throws Exception {
+        Predicate<String> isEqualsHelloWorld = message -> message.equals("Hello World");
+        Assertions.assertEquals(true, isEqualsHelloWorld.test("Hello World"));
+        Predicate<String> isNotEqualsHelloWorld = isEqualsHelloWorld.negate();
+        Assertions.assertEquals(true, isNotEqualsHelloWorld.test("Hello..."));
+    }
+
+    public interface FunctionalInterfaceSample {
+        public abstract void helloWorld(String message);
+    }
+}
